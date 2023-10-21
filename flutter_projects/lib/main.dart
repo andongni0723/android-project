cb62df941c887b4773a9978ff6f456678928c841
andongni <x102039@gmail.com>
2023-10-22 07:44:32 +0800

    add flutter_projects

---
+++ flutter_projects/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: const Text("Hello Flutter")),
+      body: const Column(
+        children: [
+          MyApp(),
+          MyButton()
+        ],
+      )),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 200,
+        height: 200,
+        alignment: Alignment.center,
+        transform: Matrix4.skewY(0.1),
+        margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+              color: Colors.red,
+              width: 10),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 10
+            )
+          ]
+        ),
+
+        child: const Text("P21", style: TextStyle(
+            color: Colors.white,
+            fontSize: 25))
+      )
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 200,
+        height: 50,
+        margin: const EdgeInsets.all(80),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const[
+              BoxShadow(
+                color: Colors.blue,
+                blurRadius: 2
+              )
+            ],
+        ),
+        child: const Text("按紐", style: TextStyle(
+            color: Colors.white,
+            fontSize: 25
+        )),
+      ),
+    );
+
+  }
+}
